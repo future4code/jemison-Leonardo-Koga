67c834e068b9f72167c248f8a1d8afc271584be6
Leonardo Koga <leokoga@gmail.com>
2022-04-12 16:36:25 -0300

    .gitignore

--- modulo1/variaveis/template_javascript/index2.js
+++
@@ -1,8 +0,0 @@
-let roupa = true;
-let comida = true;
-let sono =false;
-
-console.log("Você esta usando uma roupa azul?", roupa);
-console.log("Vocë está com fome?", comida);
-console.log("Voccê está com sono?", sono);
-
