254e0eb9e7a722ebfae6924133dd7ed7e1d3479a
Leonardo Koga <leokoga@gmail.com>
2022-04-12 11:30:30 -0300

    aula variaveis

---
+++ modulo1/variaveis/template_javascript/index2.js
@@ -0,0 +1,8 @@
+let roupa = true;
+let comida = true;
+let sono =false;
+
+console.log("Você esta usando uma roupa azul?", roupa);
+console.log("Vocë está com fome?", comida);
+console.log("Voccê está com sono?", sono);
+
---
+++ modulo1/variaveis/template_javascript/index3.js
@@ -0,0 +1,10 @@
+let a = 10;
+let b = 25;
+let c;
+
+c = a;
+a = b;
+b = c;
+
+console.log("O novo valor de a é", a);
+console.log("O novo valor de b é", b);
