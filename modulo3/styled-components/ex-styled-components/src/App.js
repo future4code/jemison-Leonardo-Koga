c83d72bed5a191616268d316997882656302157d
Leonardo Koga <leokoga@gmail.com>
2022-06-30 21:56:20 -0300

    LabZap

---
+++ modulo3/styled-components/ex-styled-components/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {MeuComponente} from './components/MeuComponente';
+import {Footer} from './components/Footer/Footer';
+
+function App(){
+  return(
+   <div>
+    <MeuComponente/>
+    <Footer/>
+   </div>
+  )
+}
+
+export default App;
---
+++ modulo3/styled-components/ex-styled-components/src/components/style.js
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+
+export const Body = styled.div`
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    flex-direction: column;
+`
+
+export const Header = styled.div`
+    display: flex;
+    justify-content: center;
+`
+export const ImgEstilizado = styled.img`
+    height: 60px;
+`
+export const HeaderEstilizado = styled.header`
+    text-align: center;
+    display: flex;
+    align-items: flex-end;
+    font-size: 30px;
+    font-weight: bold;
+`
+export const Main = styled.div`
+    display: flex;
+    height: 100vh;
+`
+export const Coluna1 = styled.div`
+    background-color: orange;
+    width: 10vw;
+`
+export const Coluna2 = styled.div`
+    width: 80vw;
+    height: 85vh;
+    display: flex;
+    align-items: flex-end;
+    justify-content: space-around;
+`
+export const ButtonEstilizado = styled.button`
+    border-radius: 10px;
+    width: 150px;
+`
+export const Coluna3 = styled.div`
+    background-color: orange;
+    width: 10vw;
+`
+
