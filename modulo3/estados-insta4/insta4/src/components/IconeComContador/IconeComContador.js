e67f4c237abf5e7a9a16e77620c33247470b963c
Leonardo Koga <leokoga@gmail.com>
2022-07-05 20:59:40 -0300

    renderizacao de listas 1

--- modulo3/estados-insta4/insta4/src/components/IconeComContador/IconeComContador.js
+++ modulo3/estados-insta4/insta4/src/components/IconeComContador/IconeComContador.js
@@ -1,16 +1,12 @@
 import React from 'react'
-import styled from 'styled-components'
+import {Container, Imagem} from './style'
 
-const IconContainer = styled.div`
-	display: flex;
-`
-const IconImage = styled.img`
-	margin-right: 5px;
-`
+
+
 
 export function IconeComContador(props) {
-	return <IconContainer>
-		<IconImage alt={'Icone'} src={props.icone} onClick={props.onClickIcone}/>
+	return <Container className='IconContainer'>
+		< Imagem className ='IconImage' alt={'Icone'} src={props.icone} onClick={props.onClickIcone}/>
 		<p>{props.valorContador}</p>
-	</IconContainer>
+	</Container>
 }
