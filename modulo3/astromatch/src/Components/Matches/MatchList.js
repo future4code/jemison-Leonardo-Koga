eda7e0818860c35a2c4d2f4a9ce5fd67693f6cde
Leonardo Koga <leokoga@gmail.com>
2022-08-28 22:27:21 -0300

    astromatch

--- modulo3/astromatch/src/Components/Matches/MatchList.js
+++ modulo3/astromatch/src/Components/Matches/MatchList.js
@@ -5,22 +5,31 @@ import MatchItem from "./MatchItem"
 function MatchList(){
     const [matches, setMatches] = useState([])
     
-
+    const getMatches = () => {
+            axios.get('https://us-central1-missao-newton.cloudfunctions.net/astroMatch/leonardo-koga-jemison/matches')
+            .then((response) => {
+                console.log(response.data.matches)
+                setMatches(response.data.matches)
+            }).catch((erro) => {
+                console.log(erro)
+            });
+    }
     useEffect(() => {
-        axios.get('https://us-central1-missao-newton.cloudfunctions.net/astroMatch/leonardo-koga-jemison/matches')
-        .then((response) => {
-            console.log(response.data.matches)
-            setMatches(response.data.matches)
-        })
-    }, [])
+        getMatches()
+    },[matches]);
 
     return(
-        <div>
-            {matches.map((profile) => {
-                return <MatchItem profile={profile}/>
-            })}
-       </div>
+        <>{getMatches ? 
+            <div>
+                {matches.map((profile) => {
+                    return <MatchItem profile={profile}/>
+                })}
+
+            </div> : <div>Nenhum match</div>
+                }
+        </>
+
     )
-}
+            }
 
 export default MatchList;
