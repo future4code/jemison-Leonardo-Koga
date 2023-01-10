70cf28adbda6672c3ef6861427e2b402eb427264
Leonardo Koga <leokoga@gmail.com>
2023-01-10 18:50:31 -0300

    revisão backend

---
+++ modulo5/revisao-full-stack/src/endpoints/stock.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from "express";
+import connection from "../database/connecttion";
+
+
+
+export const getStock = async (req: Request, res: Response) => {
+    let errorCode = 400;
+    try{
+        const stock = await connection.select('name', 'qty_stock').from('Case_Products')
+
+        res.status(200).send(stock)
+        
+    }catch(error:any){
+        res.status(errorCode).send({message:error.message});
+    }
+}
