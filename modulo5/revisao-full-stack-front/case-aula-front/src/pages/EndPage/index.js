f38bdfc786a4484cd56d9c4a5017a71c207f13db
Leonardo Koga <leokoga@gmail.com>
2023-01-12 22:52:38 -0300

    inicio front

---
+++ modulo5/revisao-full-stack-front/case-aula-front/src/pages/EndPage/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function EndPage() {
+  return (
+    <div>
+      <h1>Oi eu sou a EndPage!</h1>
+    </div>
+  );
+}
---
+++ modulo5/revisao-full-stack-front/case-aula-front/src/pages/Home/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Form from '../../components/Form';
+import Header from '../../components/Header';
+import Product from '../../components/Product';
+
+export default function home() {
+ return (
+   <div>
+        <Header/>
+        <Product/>
+        <Form/>
+   </div>
+ );
+}
