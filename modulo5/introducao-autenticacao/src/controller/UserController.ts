a82d1c4335031ac8e4cc207a43f2827af05d7035
Leonardo Koga <leokoga@gmail.com>
2023-01-31 21:40:35 -0300

    exercicio introducao a autenticacao

---
+++ modulo5/introducao-autenticacao/src/controller/UserController.ts
@@ -0,0 +1,80 @@
+import { user } from './../model/user';
+import { Request, Response } from "express";
+import { UserBusiness } from "../business/UserBusiness";
+import { EditUserInputDTO, LoginInputDTO, UserInputDTO } from "../model/user";
+import { Authenticator } from "../services/Authenticator";
+
+export class UserController {
+
+      public signup = async (req: Request, res: Response) => {
+        try {
+          const { name, nickname, email, password } = req.body;
+    
+          const input: UserInputDTO = {
+            name,
+            nickname,
+            email,
+            password,
+          };
+          const userBusiness = new UserBusiness()
+          const token = await userBusiness.signup(input);
+    
+          res.status(201).send({ message: "Usuário criado!", token });
+        } catch (error: any) {
+          res.status(400).send(error.message);
+        }
+      };    
+
+
+      public login = async (req: Request, res: Response) => {
+        try {
+          const { email, password } = req.body;
+    
+          const input: LoginInputDTO = {
+            email,
+            password,
+          };
+          const userBusiness = new UserBusiness()
+          const token = await userBusiness.login(input);
+    
+          res.status(200).send({ token });
+        } catch (error: any) {
+          res.status(400).send(error.message);
+        }
+      };    
+
+
+      public editUser = async (req: Request, res: Response) => {
+        try {
+          
+          const input: EditUserInputDTO = {
+            name: req.body.name,
+            nickname: req.body.nickname,
+            token: req.headers.authorization as string
+          };
+
+          const userBusiness = new UserBusiness()
+          await userBusiness.editUser(input);
+    
+          res.status(201).send({ message: "Usuário alterado!" });
+        } catch (error: any) {
+          res.status(400).send(error.message);
+        }
+      }; 
+ 
+      public getUser = async (req: Request, res: Response) => {
+        try {
+          const token = req.headers.authorization as string;
+
+          const userBusiness = new UserBusiness()
+
+          const autheticationData= userBusiness.getUser(token)
+
+          res.status(200).send(autheticationData)
+        } catch(error: any) {
+          res.status(400).send(error.message)
+        }
+      }
+
+
+}
---
+++ modulo5/introducao-autenticacao/src/model/user.ts
@@ -0,0 +1,40 @@
+export type user = {
+   id: string
+   email: string
+   password: string
+   name: string
+   nickname: string
+}
+
+export interface UserInputDTO {
+   name: string,
+   nickname: string,
+   email: string,
+   password: string
+}
+
+export interface LoginInputDTO {
+   email: string,
+   password: string
+}
+
+export interface EditUserInputDTO {
+   name: string,
+   nickname: string,
+   token: string
+}
+
+export interface EditUserInput {
+   name: string,
+   nickname: string,
+   id: string
+}
+
+export interface GetUserDTO {
+   id: string,
+   email: string
+}
+
+export type AuthenticationData = {
+   id: string
+}
